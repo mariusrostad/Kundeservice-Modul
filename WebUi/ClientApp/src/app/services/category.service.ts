063bcffe1533a4b732e98c62bfdb2ac124dd02be
Marius Rostad <marostad98@gmail.com>
2019-11-06 13:24:50 +0100

    Fixed semicolon bug

--- WebUi/ClientApp/src/app/services/category.service.ts
+++ WebUi/ClientApp/src/app/services/category.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Category } from '../models/category'
+import { Category } from '../models/category';
 import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
